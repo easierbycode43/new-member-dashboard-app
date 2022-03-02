f47f02bf8fb033eadb4232020c43fbc125ce51bd
EasierByCode[43] <daniel@community43.org>
2022-03-02 16:08:06 -0700

    sync

---
+++ src/views/HomePage.vue
@@ -0,0 +1,204 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Testy McTesterson</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-content>
+        <ion-card>
+          <ion-card-content class="md card-content-md" style="width: 50%; float: left;">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md">
+                  <p>Member ID</p>
+                  <h2>20801</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md">
+                  <p>Client ID</p>
+                  <h2>20801</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-header class="md card-heaion-card-header-md" style="text-align: center">
+            <ion-card-title class="md">🎨🌐 Cross-Unit 🍄🌱</ion-card-title>
+          </ion-card-header>
+
+          <ion-card-content class="md card-content-md" style="width: 50%; float: left;">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md">
+                  <p>Original liaison</p>
+                  <h2>Sara Abraham</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md" style="padding-top: 0">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md">
+                  <p>Current worker</p>
+                  <h2>Casper Ghostius</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Housing status</p>
+                  <h2>Stable</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Selected main goals</p>
+                  <h2>Benefits / Resources</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Date of First Day</p>
+                  <h2>2/24/2022</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Membership length (in days)</p>
+                  <h2>363</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Engagements in last year (multiple per day)</p>
+                  <h2>46</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Engagements in last year (unique per day)</p>
+                  <h2>41</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Date of most recent engagement</p>
+                  <h2>3/03/2022</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+
+          <ion-card-content class="md card-content-md">
+            <ion-list class="md list-md list-lines-none list-md-lines-none">
+              <ion-item class="item md">
+                <ion-label class="md" style="text-align: center">
+                  <p>Source of most recent engagement</p>
+                  <h2>Dynamic</h2>
+                </ion-label>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+        </ion-card>
+      </ion-content>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  }
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
